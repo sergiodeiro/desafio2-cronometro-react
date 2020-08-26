133fd0f69f672ebe546b8fa14aa76b7472cbfab4
Sergio Deiro <sergiodeiro@gmail.com>
2020-08-25 22:42:54 -0300

    Subindo Primeira Versão do Cronometro

---
+++ README.md
@@ -0,0 +1,19 @@
+Cronometro - React
+
+Desafio da Unifacs em fazer um cronometro - Desafio 2
+
+## Rodar o Projeto
+
+### `npm install ou yarn`
+
+No diretório do projeto, você pode executar:
+
+### `npm start`
+
+Executa o aplicativo no modo de desenvolvimento.<br>
+Abrir [http://localhost:3000](http://localhost:3000) para vê-lo no navegador.
+
+Equipe: 
+
+Sérgio Deiró, Vandeilson Figueiredo, Phillipe Carvalho, Mauricio Lanner
+
---
+++ src/pages/Main/index.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import DisplayComponent from '../../components/Display';
+import BtnComponent from '../../components/Button';
+import '../../assets/css/style.css';
+
+function Main() {
+  const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+
+  const run = () => {
+    if(updatedM === 60){
+      updatedH++;
+      updatedM = 0;
+    }
+    if(updatedS === 60){
+      updatedM++;
+      updatedS = 0;
+    }
+    if(updatedMs === 100){
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0})
+  };
+
+  const resume = () => start();
+
+
+  return (
+    <div className="main-section">
+     <div className="clock-holder">
+          <div style={{textAlign: 'center'}}>
+            <h2>Cronomêtro - React</h2>
+          </div>
+          <div className="stopwatch">
+               <DisplayComponent time={time}/>
+               <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start}/>
+          </div>
+     </div>
+    </div>
+  );
+}
+
+export default Main;
