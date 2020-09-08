4565e07a387f9d6704f6b26ff72362508d887760
Sergio Deiro <sergiodeiro@gmail.com>
2020-09-08 20:04:05 -0300

    Hour Component space

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -6,12 +6,17 @@ const Display = props => {
      if(props.time.h === 0){
        return '';
      } else {
-       return <Span>{(props.time.h >= 10)? props.time.h : "0"+ props.time.h}</Span>;
+       return (
+        <>
+        <Span>{(props.time.h >= 10)? props.time.h : "0"+ props.time.h}</Span>&nbsp;:&nbsp;
+        </>
+       )
+       ;
      }
   }
   return (
     <Div>
-       {h()}&nbsp;:&nbsp;
+       {h()}
        <Span>{(props.time.m >= 10)? props.time.m : "0"+ props.time.m}</Span>&nbsp;:&nbsp;
        <Span>{(props.time.s >= 10)? props.time.s : "0"+ props.time.s}</Span>&nbsp;:&nbsp;
        <Span>{(props.time.ms >= 10)? props.time.ms : "0"+ props.time.ms}</Span>
